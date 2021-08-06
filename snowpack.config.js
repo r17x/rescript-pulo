101487268d31b860dd04806e18675185f564dfb2
RiN <ri7nz@evilfactory.id>
2021-08-06 18:42:20 +0800

    perf(Build): optimize build stage with @snowpack/plugin-webpack (#3)
    * [Before](https://www.webpagetest.org/result/210806_AiDc5K_9e0333b4fe161e7c85965024a01413b7/)
    * [After](https://www.webpagetest.org/result/210806_BiDc0Q_55264bacdf2ea682c96789ed0e8a5e94/)

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,14 +1,15 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    public: { url: '/', static: true },
-    src: { url: '/dist' },
+    public: { url: "/", static: true },
+    src: { url: "/dist" },
   },
   exclude: ["**/*.{res,resi}"],
   plugins: [
-    '@snowpack/plugin-react-refresh',
-    '@snowpack/plugin-dotenv',
-    '@jihchi/plugin-rescript',
+    "@snowpack/plugin-react-refresh",
+    "@snowpack/plugin-dotenv",
+    "@jihchi/plugin-rescript",
+    "@snowpack/plugin-webpack",
   ],
   packageOptions: {
     /* ... */
